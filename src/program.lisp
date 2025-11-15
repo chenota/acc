51cc0bd528d5c5580f106036f0cc0beb48204100
Alex Chenot <alex@chenot.dev>
2025-11-15 15:51:40 -0700

    feat: program parser returns output

--- src/program.lisp
+++ src/program.lisp
@@ -1,11 +1,26 @@
 (in-package :acc)
 
-(defmacro with-restore-and (seq &body forms)
-  "Evaluate FORMS in sequence. If any form in FORMS returns NIL, stop execution and restore the state of SEQ."
-  `(let ((pos (capture ,seq))
-         (x (and ,@forms)))
-     (unless x (restore ,seq pos))
-     x))
+(defmacro peg-rules (func seq &body symbols-and-forms)
+  "Evaluate FUNC with arguments symbols-and-forms. If FUNC returns NIL, stop execution and restore the state of SEQ."
+  (loop
+ with sym = nil
+ with flist = nil
+ with vlist = (make-hash-table)
+ for item in symbols-and-forms
+ do (cond
+     ((symbolp item) (setq sym item))
+     ((and (consp item) (null sym)) (setf flist (cons item flist)))
+     ((consp item) (push `(setq ,sym ,item) flist)
+                   (setf (gethash sym vlist) t)
+                   (setf sym nil))
+     (t (error "expected a symbol or compound form")))
+ finally
+   (return
+     `(let (,@(loop for sym being the hash-keys of vlist collect (list sym nil)))
+        (let ((pos (capture ,seq))
+              (val (,func ,@(reverse flist))))
+          (unless val (restore ,seq pos))
+          val)))))
 
 (defmacro with-nil-error (form)
   "If FORM signals an error, return NIL."
@@ -22,21 +37,24 @@
         (error "bad"))))
 
 (defun program-rule (seq)
-  (with-restore-and seq
-    (function-rule seq)
-    (expect seq :ENDMARKER)))
+  (peg-rules and seq
+    func (function-rule seq)
+    (expect seq :ENDMARKER)
+    (list :program func)))
 
 (defun function-rule (seq)
-  (with-restore-and seq
+  (peg-rules and seq
     (expect seq :func)
-    (expect seq :ident)
-    (expect seq :ident)
+    fname (expect seq :ident)
+    ftype (expect seq :ident)
     (expect seq :lbrace)
-    (stmt-rule seq)
-    (expect seq :rbrace)))
+    expr (stmt-rule seq)
+    (expect seq :rbrace)
+    (list :func (token-value fname) (token-value ftype) expr)))
 
 (defun stmt-rule (seq)
-  (with-restore-and seq
+  (peg-rules and seq
     (expect seq :return)
-    (with-nil-error (expr-bp seq 0))
-    (expect seq :semi)))
+    expr (with-nil-error (expr-bp seq 0))
+    (expect seq :semi)
+    (list :return expr)))
--- test/program-test.lisp
+++ test/program-test.lisp
@@ -2,8 +2,16 @@
 
 (fiveam:def-suite program)
 
-(test test-main-func
-  (is (parse-program (make-token-sequence (tokenize "func main int { return 0; }")))))
+(test test-program
+  (is (eq :program (car (parse-program (make-token-sequence (tokenize "func main int { return 0; }")))))))
+
+(test test-func
+  (is (eq :func (car (function-rule (make-token-sequence (tokenize "func main int { return 0; }"))))))
+  (is (string= "main" (cadr (function-rule (make-token-sequence (tokenize "func main int { return 0; }"))))))
+  (is (string= "int" (caddr (function-rule (make-token-sequence (tokenize "func main int { return 0; }")))))))
+
+(test test-stmt
+  (is (eq :return (car (stmt-rule (make-token-sequence (tokenize "return 0;")))))))
 
 (test test-err
   (signals error (parse-program (make-token-sequence (tokenize "return return return")))))
