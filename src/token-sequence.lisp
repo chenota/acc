c5ce2709a4cd2deac40c86784da5dea0f62bf61c
Alex Chenot <alex@chenot.dev>
2025-11-14 21:08:58 -0700

    feat: add capture and restore to token sequence

--- src/token-sequence.lisp
+++ src/token-sequence.lisp
@@ -4,7 +4,8 @@
     ((location :accessor location :initform 0 :type (integer 0 *))
      (token-list :accessor token-list :initarg :token-list)
      (end-row :accessor end-row :initform 0 :type (integer 0 *))
-     (end-col :accessor end-col :initform 0 :type (integer 0 *)))
+     (end-col :accessor end-col :initform 0 :type (integer 0 *))
+     (length :accessor len :type (integer 0 *)))
   (:documentation "Token sequence manager."))
 
 (defmethod initialize-instance :after ((ts token-sequence) &key token-list &allow-other-keys)
@@ -12,10 +13,11 @@
   (assert (typep token-list 'sequence))
   (assert (every #'token-p token-list))
   (setf (token-list ts) (coerce token-list 'vector))
-  (when (> (length (token-list ts)) 0)
+  (setf (len ts) (length (token-list ts)))
+  (when (> (len ts) 0)
         (let ((last-token (aref
                               (token-list ts)
-                            (1- (length (token-list ts))))))
+                            (1- (len ts)))))
           (setf (end-row ts) (+ (token-row last-token) (token-len last-token)))
           (setf (end-col ts) (token-col last-token)))))
 
@@ -25,7 +27,7 @@
 
 (defmethod peek ((ts token-sequence))
   "Return the token at the curren position. Returns an ENDMARKER if at the end of the sequence."
-  (if (>= (location ts) (length (token-list ts)))
+  (if (= (location ts) (len ts))
       (make-token :kind :ENDMARKER :row (end-row ts) :col (end-col ts) :len 0)
       (aref (token-list ts) (location ts))))
 
@@ -34,3 +36,12 @@
   (let ((current-token (peek ts)))
     (unless (eq (token-kind current-token) :ENDMARKER) (incf (location ts)))
     current-token))
+
+(defmethod capture ((ts token-sequence))
+  "Capture the current position."
+  (location ts))
+
+(defmethod restore ((ts token-sequence) pos)
+  "Restore a previously captured position."
+  (assert (<= pos (len ts)))
+  (setf (location ts) pos))
--- test/token-sequence-test.lisp
+++ test/token-sequence-test.lisp
@@ -29,3 +29,10 @@
     (is (= 0 (token-value (advance (make-token-sequence raw-seq)))))
     (is (= 1 (token-value (let ((s (make-token-sequence raw-seq))) (advance s) (advance s)))))
     (is (eq :ENDMARKER (token-kind (let ((s (make-token-sequence raw-seq))) (dotimes (i raw-seq-len) (advance s)) (advance s)))))))
+
+(test test-capture-restore
+  (with-fixture token-sequence-test-env ()
+    (is (= 0 (capture (make-token-sequence raw-seq))))
+    (is (= 1 (let ((s (make-token-sequence raw-seq))) (advance s) (capture s))))
+    (is (= raw-seq-len (let ((s (make-token-sequence raw-seq))) (restore s raw-seq-len) (capture s))))
+    (signals error (restore (make-token-sequence raw-seq) (1+ raw-seq-len)))))
