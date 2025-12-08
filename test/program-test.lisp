1c96c1fef5f8db269bc3a6916ae8c01ae112b2a8
Alex Chenot <alex@chenot.dev>
2025-12-08 06:12:27 -0700

    test: updated program parser tests

--- test/program-test.lisp
+++ test/program-test.lisp
@@ -3,15 +3,15 @@
 (fiveam:def-suite program)
 
 (test test-program
-  (is (eq :program (car (parse-program (make-token-sequence (tokenize "func main int { return 0; }")))))))
+  (is (eq :program (car (parse-program (make-token-sequence (tokenize "func main int { return 0; }"))))))
+  (signals error (parse-program (make-token-sequence (tokenize "return 0;")))))
 
 (test test-func
   (is (eq :func (car (function-rule (make-token-sequence (tokenize "func main int { return 0; }"))))))
   (is (string= "main" (cadr (function-rule (make-token-sequence (tokenize "func main int { return 0; }"))))))
-  (is (string= "int" (caddr (function-rule (make-token-sequence (tokenize "func main int { return 0; }")))))))
+  (is (string= "int" (caddr (function-rule (make-token-sequence (tokenize "func main int { return 0; }"))))))
+  (signals error (parse-program (make-token-sequence (tokenize "func { return 0; }")))))
 
 (test test-stmt
-  (is (eq :return (car (stmt-rule (make-token-sequence (tokenize "return 0;")))))))
-
-(test test-err
+  (is (eq :return (car (stmt-rule (make-token-sequence (tokenize "return 0;"))))))
   (signals error (parse-program (make-token-sequence (tokenize "return return return")))))
