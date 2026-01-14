1aa1cb4f26b7ee160cc91707bceb6b6b8e0d5114
Alex Chenot <alex@chenot.dev>
2026-01-13 22:56:17 -0700

    fix: update coverage ignore in lexer

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -1,34 +1,34 @@
 (in-package :acc)
 
-(defstruct token
-  (kind nil :type keyword)
-  (value nil :type t)
-  (row nil :type (integer 0 *))
-  (col nil :type (integer 0 *))
-  (len nil :type (integer 0 *)))
-
 (with-ignore-coverage
- (defparameter
-   +compiled-tokens+
-   (mapcar
-       (lambda
-           (token)
-         (list
-          (first token)
-          (cl-ppcre:create-scanner
-           (concatenate 'string "^" (second token)))
-          (third token)))
-       `((:func "func" t)
-         (:return "return" t)
-         (:semi ";" t)
-         (:lbrace "\\{" t)
-         (:rbrace "\\}" t)
-         (:lparen "\\(" t)
-         (:rparen "\\)" t)
-         (:ident "[a-z][a-z0-9]*" identity)
-         (:int "[0-9]+" parse-integer)
-         (:whitespace " " nil)
-         (:newline "\\n" nil)))))
+  (defstruct token
+    (kind nil :type keyword)
+    (value nil :type t)
+    (row nil :type (integer 0 *))
+    (col nil :type (integer 0 *))
+    (len nil :type (integer 0 *)))
+
+  (defparameter
+    +compiled-tokens+
+    (mapcar
+        (lambda
+            (token)
+          (list
+           (first token)
+           (cl-ppcre:create-scanner
+             (concatenate 'string "^" (second token)))
+           (third token)))
+        `((:func "func" t)
+          (:return "return" t)
+          (:semi ";" t)
+          (:lbrace "\\{" t)
+          (:rbrace "\\}" t)
+          (:lparen "\\(" t)
+          (:rparen "\\)" t)
+          (:ident "[a-z][a-z0-9]*" identity)
+          (:int "[0-9]+" parse-integer)
+          (:whitespace " " nil)
+          (:newline "\\n" nil)))))
 
 (defun tokenize (target)
   "Transform a string into a sequence of tokens."
@@ -59,13 +59,13 @@
                              (if
                               (third matched-rule)
                               (make-token
-                               :kind (first matched-rule)
-                               :value (handler-case
-                                          (funcall (third matched-rule) match)
-                                        (undefined-function (e) (declare (ignore e)) match))
-                               :row row
-                               :col col
-                               :len (length match)))
+                                :kind (first matched-rule)
+                                :value (handler-case
+                                           (funcall (third matched-rule) match)
+                                         (undefined-function (e) (declare (ignore e)) match))
+                                :row row
+                                :col col
+                                :len (length match)))
                            (if
                             (eq (first matched-rule) :newline)
                             (progn (setf row 0) (incf col))
