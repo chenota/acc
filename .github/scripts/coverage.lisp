a706fe7888ddbcbe5e1ab7bc237c8f56a0dd3e40
Alex Chenot <alex@chenot.dev>
2026-02-04 22:02:27 -0700

    refactor: update coverage test

--- .github/scripts/coverage.lisp
+++ .github/scripts/coverage.lisp
@@ -1,53 +1,44 @@
-;; Load necessary packages
-(require 'asdf)
+;; Require necessary packages
 (require 'sb-cover)
-(ql:quickload :fiveam)
-(ql:quickload :cl-ppcre)
-(ql:quickload :unix-opts)
-;; Load acc w/ coverage data compiler flag
-(declaim (optimize sb-cover:store-coverage-data))
-(asdf:oos 'asdf:load-op :acc :force t)
-(declaim (optimize (sb-cover:store-coverage-data 0)))
+;; Load entire test system
 (ql:quickload :acc/test)
+;; Recompile just the acc system with coverage flag
+(declaim (optimize sb-cover:store-coverage-data))
+(asdf:compile-system :acc :force t)
+(asdf:load-system :acc)
+(declaim (optimize (sb-cover:store-coverage-data 0)))
 ;; Run all tests
-(handler-case (fiveam:run-all-tests)
-  (condition (c)
-             (declare (ignore c))
-             (with-open-file
-                 (stream "shields.txt"
-                         :direction :output
-                         :if-exists :supersede
-                         :if-does-not-exist :create)
-               (format stream "https://img.shields.io/badge/coverage-fail-red"))
-             (sb-ext:quit)))
+(unless (fiveam:run-all-tests)
+  (with-open-file
+      (stream "shields.txt"
+              :direction :output
+              :if-exists :supersede
+              :if-does-not-exist :create)
+    (format stream "https://img.shields.io/badge/coverage-fail-red"))
+  (sb-ext:quit :unix-status 1))
 ;; Calculate expression coverage
-(defvar
-  cov-perc
+(defparameter
+  coverage-value
   (loop
  with exp-count = 0
  with cov-count = 0
  for file in (sb-cover:save-coverage)
-   when (cl-ppcre:scan "src/(?!main\\.lisp).*\\.lisp$" (car file))
+   when (cl-ppcre:scan "src/.+/.*\\.lisp$" (car file))
  do (loop for expr in (cdr file)
             when (typep (first (car expr)) 'integer)
           do (progn
               (incf exp-count)
               (when (eq (cdr expr) t) (incf cov-count))))
- finally (progn
-          (format t "~D out of ~D expressions covered~%" cov-count exp-count)
-          (return (* 100.0 (/ cov-count exp-count))))))
+ finally (return (* 100.0 (/ cov-count exp-count)))))
 ;; Save shields URL to file
-(with-open-file (stream "shields.txt"
-                        :direction :output
-                        :if-exists :supersede
-                        :if-does-not-exist :create)
+(with-open-file (stream "shields.txt" :direction :output :if-does-not-exist :create)
   (format
       stream
       "https://img.shields.io/badge/coverage-~D%25-~A"
-    (round cov-perc)
+    (round coverage-value)
     (cond
-     ((>= cov-perc 90.0) "green")
-     ((>= cov-perc 70.0) "yellow")
+     ((>= coverage-value 90.0) "green")
+     ((>= coverage-value 70.0) "yellow")
      (t "red"))))
 ;; Done
 (sb-ext:quit)
