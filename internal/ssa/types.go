a86c3690c7fc110e7ae91dfb0a94d0fa55554118
Alex Chenot <alex@chenot.dev>
2026-05-28 20:44:48 -0600

    refactor: move func

--- internal/ssa/types.go
+++ internal/ssa/types.go
@@ -36,3 +36,25 @@ type Block struct {
 
 	Control *Value
 }
+
+type Func struct {
+	Name   string
+	Blocks []*Block
+	Entry  *Block
+
+	valueId int
+	blockId int
+}
+
+func (f *Func) newValue(op Op, t *types.Type, b *Block) *Value {
+	v := &Value{Id: f.valueId, Op: op, Type: t, Block: b}
+	f.valueId += 1
+	return v
+}
+
+func (f *Func) newBlock() *Block {
+	b := &Block{Id: f.blockId}
+	f.blockId += 1
+	f.Blocks = append(f.Blocks, b)
+	return b
+}
