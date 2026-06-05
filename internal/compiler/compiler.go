c5164efb4ae3d81ac987ce8cdc9f0a0ac7587d4d
Alex Chenot <alex@chenot.dev>
2026-06-04 22:36:26 -0600

    refactor: move compiler config

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -62,16 +62,3 @@ func Compile(r io.Reader, w io.Writer, opts ...Option) error {
 
 	return nil
 }
-
-type compilerOptions struct {
-	isAssembly bool
-}
-
-type Option func(*compilerOptions)
-
-// WithAssemblyOnly tells acc to emit text assembly rather than a linked binary.
-func WithAssemblyOnly() Option {
-	return func(o *compilerOptions) {
-		o.isAssembly = true
-	}
-}
