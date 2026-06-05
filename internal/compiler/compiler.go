486fce4f93407cf021f1ad4032ae16517f8dbda8
Alex Chenot <alex@chenot.dev>
2026-06-04 21:17:33 -0600

    feat: compiler orchestration

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -1,9 +1,41 @@
 package compiler
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"github.com/chenota/acc/internal/asmtxt"
+	"github.com/chenota/acc/internal/codegen"
+	"github.com/chenota/acc/internal/lexer"
+	"github.com/chenota/acc/internal/parser"
+	"github.com/chenota/acc/internal/semantic"
+	"github.com/chenota/acc/internal/ssa"
+)
 
 // Compile is the top-level function of the acc compiler.
 // It orchestrates all the compiler's important components.
 func Compile(r io.Reader, w io.Writer) error {
+	tokens, err := lexer.Tokenize(r)
+	if err != nil {
+		return err
+	}
+	ast, err := parser.ParseProgram(tokens)
+	if err != nil {
+		return err
+	}
+	if err := semantic.Analyze(ast); err != nil {
+		return err
+	}
+	ssaValues, err := ssa.BuildAndAllocate(ast)
+	if err != nil {
+		return err
+	}
+	instructions := codegen.GenerateProgram(ssaValues)
+	for _, instr := range asmtxt.Stringify(instructions) {
+		_, err := fmt.Fprintln(w, instr)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
