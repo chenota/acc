668ecc35e80f4fb8ddf4837491c96d141b38e207
Alex Chenot <alex@chenot.dev>
2026-06-19 06:35:54 -0600

    test: overflow test for constant folding

--- internal/ssa/ssa_test.go
+++ internal/ssa/ssa_test.go
@@ -1,6 +1,8 @@
 package ssa
 
 import (
+	"fmt"
+	"math"
 	"strings"
 	"testing"
 
@@ -103,4 +105,35 @@ func TestGenSsa_DivideByZero(t *testing.T) {
 
 	require.NotNil(t, b.Control)
 	assert.Equal(t, OpDivide, b.Control.Op)
+}
+
+func TestGenSsa_AdditionOverflow(t *testing.T) {
+	inputStr := fmt.Sprintf(`fun main () -> int { return %d + 1; }`, math.MaxInt32)
+	tokens, err := lexer.Tokenize(strings.NewReader(inputStr))
+	require.NoError(t, err)
+
+	funcs, err := parser.ParseProgram(tokens)
+	require.NoError(t, err)
+
+	require.NoError(t, semantic.Analyze(funcs))
+
+	// not really necessary but ensuring the RAX is the return register
+	returnRegister := register.RegA
+	compiledFuncs, err := BuildAndAllocate(funcs, WithReturnRegister(returnRegister))
+	require.NoError(t, err)
+
+	require.Len(t, compiledFuncs, 1)
+	f := compiledFuncs[0]
+
+	assert.Equal(t, "main", f.Name)
+
+	require.Len(t, f.Blocks, 1)
+	b := f.Blocks[0]
+
+	assert.Equal(t, BlockRet, b.Kind)
+
+	require.NotNil(t, b.Control)
+	assert.Equal(t, OpLiteral, b.Control.Op)
+	assert.Equal(t, types.KInt32, b.Control.Type.Kind)
+	assert.Equal(t, int32(math.MinInt32), b.Control.Value)
 }
