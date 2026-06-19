fbfe37f8e33ab97d5c774cc2252713f99bd5be3c
Alex Chenot <alex@chenot.dev>
2026-06-18 20:59:47 -0600

    feat: unoptimized ssa for bops

--- internal/ssa/types.go
+++ internal/ssa/types.go
@@ -14,6 +14,10 @@ const (
 	OpConstInt32
 	OpStoreReg
 	OpLoadReg
+	OpAdd
+	OpSubtract
+	OpMultiply
+	OpDivide
 )
 
 type Value struct {
--- internal/ssa/unoptimized.go
+++ internal/ssa/unoptimized.go
@@ -68,7 +68,51 @@ func (b *builder) genExpr(expr *ir.Node) (*Value, error) {
 		default:
 			return nil, diagnostic.NewError(fmt.Sprintf("unknown integer type: %v", expr.Type), expr.Pos)
 		}
+	case ir.OpPlus, ir.OpMinus, ir.OpTimes, ir.OpDiv:
+		return b.genBop(expr)
 	default:
 		return nil, diagnostic.NewError(fmt.Sprintf("unknown expression operation: %d", expr.Op), expr.Pos)
+	}
+}
+
+func (b *builder) genBop(expr *ir.Node) (*Value, error) {
+	if len(expr.List) != 2 {
+		return nil, diagnostic.NewError("binary operator without two operands", expr.Pos)
+	}
+	left := expr.List[0]
+	right := expr.List[1]
+
+	leftVal, err := b.genExpr(left)
+	if err != nil {
+		return nil, err
+	}
+
+	rightVal, err := b.genExpr(right)
+	if err != nil {
+		return nil, err
+	}
+
+	switch expr.Type.Kind {
+	case types.KInt32:
+		v := b.targetFunc.newValue(intBopFrom(expr), expr.Type, b.currentBlock)
+		v.Args = []*Value{leftVal, rightVal}
+		return v, nil
+	default:
+		return nil, diagnostic.NewError(fmt.Sprintf("cannot perform addition for type %v", expr.Type), expr.Pos)
+	}
+}
+
+func intBopFrom(n *ir.Node) Op {
+	switch n.Op {
+	case ir.OpPlus:
+		return OpAdd
+	case ir.OpMinus:
+		return OpSubtract
+	case ir.OpTimes:
+		return OpMultiply
+	case ir.OpDiv:
+		return OpDivide
+	default:
+		return OpUnknown
 	}
 }
