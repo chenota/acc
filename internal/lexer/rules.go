c93c5ef0226ff283c63396f00a254e61436dbb9c
Alex Chenot <alex@chenot.dev>
2026-06-21 14:05:12 -0600

    feat: equal and let in lexer

--- internal/lexer/rules.go
+++ internal/lexer/rules.go
@@ -21,6 +21,8 @@ const (
 	KindMinus
 	KindStar
 	KindDiv
+	KindLetKw
+	KindEqual
 )
 
 type tokenRule struct {
@@ -32,6 +34,7 @@ var rules = []tokenRule{
 	{KindFunKw, mustAnchor(`fun`)},
 	{KindIntKw, mustAnchor(`int`)},
 	{KindReturnKw, mustAnchor(`return`)},
+	{KindLetKw, mustAnchor(`let`)},
 	{KindComma, mustAnchor(`,`)},
 	{KindArrow, mustAnchor(`->`)},
 	{KindLBracket, mustAnchor(`{`)},
@@ -42,6 +45,7 @@ var rules = []tokenRule{
 	{KindMinus, mustAnchor(`-`)},
 	{KindStar, mustAnchor(`\*`)},
 	{KindDiv, mustAnchor(`/`)},
+	{KindEqual, mustAnchor(`=`)},
 	{KindSemicolon, mustAnchor(`;`)},
 	{KindInteger, mustAnchor(`-?[0-9]+(_[0-9]+)*`)},
 	{KindIdentifier, mustAnchor(`[a-zA-Z_][a-zA-Z0-9_]*`)},
