5c0fe6cfd2901207d349c9252e77e6a168f0bad8
Alex Chenot <alex@chenot.dev>
2026-07-03 12:21:57 -0600

    feat: unused for now new way of representing registers

--- internal/register/register.go
+++ internal/register/register.go
@@ -20,3 +20,26 @@ const (
 	Reg14
 	Reg15
 )
+
+// Mask returns a mask containing just the Register
+func (r Register) Mask() Mask {
+	return 1 << r
+}
+
+type Mask uint64
+
+// NewMask creates a new mask containing a set of registers
+func NewMask(regs ...Register) Mask {
+	var m Mask
+	for _, r := range regs {
+		m |= r.Mask()
+	}
+	return m
+}
+
+var (
+	CallerSaved  = NewMask(RegA, RegC, RegD, RegSI, RegDI, Reg8, Reg9, Reg10, Reg11)
+	CalleeSaved  = NewMask(RegB, Reg12, Reg13, Reg14, Reg15)
+	Reserved     = NewMask(RegSP, RegBP)
+	ReturnTarget = RegA
+)
